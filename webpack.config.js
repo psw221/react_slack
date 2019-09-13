e0702fcc61f8576d39bc28ec0c0e6abc8d25d1d5
unknown <tpdnddlek@gmail.com>
2019-09-13 16:36:07 +0900

    기본 화면 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
+const webpack = require('webpack');
 const path = require("path");
 
-module.exports = {
-  entry: "./src/js/app.js",
+module.exports = [{
+  cache: true,
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+  },
+  context: path.resolve(__dirname, 'src'),
+  entry: './Routes.tsx',
   output: {
-    path: path.resolve(__dirname, "dist/js"),
-    filename: "bundle.js"
+    path: __dirname + "/dist/js",
+    filename: 'build.js',
+    publicPath: '/dist/js',
+  },
+  module: {
+    rules: [
+        {
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: ['ts-loader'],
+        },
+    ]
+},
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.ts', '.tsx', '.js']
   }
-};
+}];
